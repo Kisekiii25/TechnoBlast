94b861739a50c6e8ac0f4bd569f2a18abf29f2b4
Keivin <129949532+Kisekiii25@users.noreply.github.com>
2024-12-03 23:14:07 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// FOR NAVIGATOR
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
+
+// FOR SWIPER IMAGE
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    710: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+
+
+var modal = document.getElementById("myModal");
+var buttons = document.querySelectorAll(".myBtn"); // Get all buttons with class "myBtn"
+var span = document.getElementsByClassName("close")[0];
+
+// Add event listener to each button to open the modal
+buttons.forEach(function(btn) {
+  btn.onclick = function() {
+    modal.style.display = "block";
+  };
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
